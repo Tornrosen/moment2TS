e9d9610b4bec111c3fed3a2d799e9d78fd7905b4
Tornrosen <elec11anna@hotmail.com>
2025-04-11 15:59:14 +0200

    fortsätta med klassmetoder och TS-kod

--- src/main.ts
+++ src/main.ts
@@ -35,42 +35,115 @@ function toggleMenu() {
 
 let inputTaskEl = document.getElementById("taskText") as HTMLInputElement;
 let inputPrioEl = document.getElementById("priority") as HTMLInputElement;
+let taskListEl = document.getElementById("taskList") as HTMLUListElement;
+let submitBtnEl = document.getElementById("submitBtn") as HTMLButtonElement;
+let i;
+
 
 //skapar interface för todo
 
 interface Todo {
   task:string,
   completed:boolean,
-  priority:number,
-  addToDo():boolean
+  priority:string,
 }[]
 
 //skapa klass med array, constructor och metoder
 
 class TodoList implements Todo {
 
-  todos: Todo = {
-    task: inputTaskEl.value,
-    completed: false,
-    priority: inputPrioEl.value
-  } 
+  todos: Todo[] = [
+    { task: inputTaskEl.value, completed: false, priority: inputPrioEl.value }
+  ];
 
-  task:string = inputTaskEl.value;
-  completed:boolean = false;
-  priority:number = inputPrioEl.value;
+  task:string;
+  completed:boolean;
+  priority:string;
+
   
-  constructor(task:string, completed: boolean, priority:number) {
+  constructor(task:string, completed: boolean, priority:string) {
     this.task=task;
     this.completed=completed;
     this.priority=priority;
+    this.getTodos();
   }
-
-  addToDo():boolean {
-if(this.task.length>=1&&this.priority===1||this.priority===2||this.priority===3)
+ 
+  addToDo(task:string, priority:string):boolean {
+if(task.length>=1&&priority==="1"||priority==="2"||priority==="3")
    {
-    return true;
+    return true
   }
   else {
     return false
   }
-}}
+}
+/*markToDoCompleted(todoIndex:number):void {
+ if(this.todos.indexOf) this.todos.indexOf=todoIndex;
+
+  let checkBox = document.getElementById("checkbox") as HTMLInputElement;
+  if (checkBox.checked=true) {
+  this.completed=true;
+  }
+  else {this.completed=false}
+}*/
+
+//get todos - se get och set
+getTodos(){
+  return "Här är vi!"
+}
+
+saveToLocalStorage(): void {
+  let tasks = document.getElementsByClassName("task");
+  let tempArr = [];
+
+  for (i = 0; i < tasks.length; i++) {
+    tempArr.push(tasks[i].innerHTML);
+  }
+
+  let jsonStr = JSON.stringify(tempArr);  
+
+  localStorage.setItem("tasks", jsonStr);
+  console.log(tasks);
+}
+
+loadFromLocalStorage() : void {
+  if (localStorage) {
+    let savedTasks = localStorage.getItem("tasks");
+
+    if (savedTasks) {
+      let objTasks = JSON.parse(savedTasks);
+
+      if (taskListEl) {
+        for (i = 0; i < objTasks.length; i++) {
+          let newEl = document.createElement("article");
+          let newTextNode = document.createTextNode(objTasks[i]);
+          newEl.appendChild(newTextNode);
+          newEl.className = "task";
+          taskListEl.appendChild(newEl);
+        }
+      }
+    };
+  }
+}
+}
+
+function addTodo():void {
+  //köra metoden i klassen, addToDo, syntax?
+  if (taskListEl) {
+    let newEl = document.createElement("article");
+    let newTextNode = document.createTextNode(`Uppgift: Förstå TypeScript. Prioritet: 1. 
+  Avklarad?`);
+    newEl.appendChild(newTextNode);
+    taskListEl.appendChild(newEl);
+    newEl.className = "task";
+    inputTaskEl.value = "";
+    inputPrioEl.value = "";
+  }
+}
+
+//skapa händelsehanterare
+
+submitBtnEl.addEventListener("click", addTodo);
+
+
+
